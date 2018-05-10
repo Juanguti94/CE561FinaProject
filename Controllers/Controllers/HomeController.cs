af74e736e179f9a0dbb3d0b08d0431e1fe2d8383
Juanguti94 <39076034+Juanguti94@users.noreply.github.com>
2018-05-10 00:03:44 -0700

    Add files via upload

---
+++ Controllers/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "This Website holds customer's past movie rentals.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Feel Free to contact me with any\n questions or concerns about the site.";
+
+            return View();
+        }
+        public IActionResult Movies()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
